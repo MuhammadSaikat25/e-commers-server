7bfae750a55b4494b251963d42612bac788d6c78
Muhammad Saikat <smsaikat000@gmail.com>
2024-01-03 17:25:16 +0600

    added route for accept and reject seller request

--- index.js
+++ index.js
@@ -52,7 +52,17 @@ async function run() {
     const AddToCart = client.db("TecHouse").collection("AddToCart");
     const SellerRequest = client.db("TecHouse").collection("SellerRequest");
     await client.db("admin").command({ ping: 1 });
- 
+    //! ---------------------------Verify admin
+    const VerifyAdmin = async (req, res, next) => {
+      const email = req.decode.email;
+      const query = { email };
+      const findUser = await Users.findOne(query);
+      const admin = findUser.role === "admin";
+      if (!admin) {
+        return res.status(401).send("forbidden access");
+      }
+      next();
+    };
     //* ----------------- user related route --------------------
     // ! post user into data base
     app.post("/postUser", async (req, res) => {
@@ -60,40 +70,106 @@ async function run() {
       const result = await Users.insertOne(data);
       res.send(result);
     });
-    app.get('/getUser/:email',async(req,res)=>{
-      const email=req.params.email 
-      const query={email}
-      const result=await Users.findOne(query)
-      res.send(result)
-    })
+    // ! get a single user
+    app.get("/getUser/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email };
+      const result = await Users.findOne(query);
+      res.send(result);
+    });
+    // ! make user to seller
+    app.patch(
+      "/makeUserToSeller/:email",
+      VerifyJwt,
+      VerifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        const query = { email };
+        const updateDoc = {
+          $set: {
+            role: "seller",
+          },
+        };
+        const result = await Users.updateOne(query, updateDoc);
+        res.send(result);
+      }
+    );
+    app.patch(
+      "/makeUserToStillUser/:email",
+      VerifyJwt,
+      VerifyAdmin,
+      async (req, res) => {
+        const email = req.params.email;
+        const query = { email };
+        const updateDoc = {
+          $set: {
+            role: "user",
+          },
+        };
+        const result = await Users.updateOne(query, updateDoc);
+        res.send(result);
+      }
+    );
     // * -------------------- Products related route --------------
     // ! get all Products
-    app.get("/getAllProducts",async (req, res) => {
+    app.get("/getAllProducts", async (req, res) => {
       const result = await Products.find().toArray();
       res.send(result);
     });
     // ! get single product
-    app.get('/singleProduct/:id',VerifyJwt,async(req,res)=>{
-      const id=req.params.id 
-      const query={_id:new ObjectId(id)}
-      const result=await Products.findOne(query)
-      res.send(result)
-    })
+    app.get("/singleProduct/:id", VerifyJwt, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await Products.findOne(query);
+      res.send(result);
+    });
 
     // * -------------------- Add To Card related route ------------------------
-    app.post('/addToCard/:id',VerifyJwt,async(req,res)=>{
-      const data=req.body
-      const result=await AddToCart.insertOne(data)
-      res.send(result)
-    })
+    // ! add cart data into database
+    app.post("/addToCard/:id", VerifyJwt, async (req, res) => {
+      const data = req.body;
+      const result = await AddToCart.insertOne(data);
+      res.send(result);
+    });
     // * ---------------------- Seller Related Route ---------------------
-    // ! applySeller
-    app.post('/applySeller',async(req,res)=>{
-      const data=req.body
-      const result=await SellerRequest.insertOne(data)
-      res.send(result)
-    })
+    // ! put Seller request
+    app.put("/applySeller/:email", async (req, res) => {
+      const data = req.body;
+      const email = req.params.email;
+      const query = { email };
+      const findData = await SellerRequest.findOne(query);
+      if (findData) {
+        return;
+      }
+      const result = await SellerRequest.insertOne(data);
+      res.send(result);
+    });
+    // ! get All Seller Request
+    app.get("/getSellerRequest", VerifyJwt, VerifyAdmin, async (req, res) => {
+      const result = await SellerRequest.find().toArray();
+      res.send(result);
+    });
+    // ! accept and reject seller request
+    app.patch(
+      "/acceptRequest/:id",
+      VerifyJwt,
+      VerifyAdmin,
+      async (req, res) => {
+        const data = req.body;
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const updateDoc = {
+          $set: {
+            ...data,
+          },
+        };
+        const result = await SellerRequest.updateOne(query, updateDoc);
+
+        res.send(result);
+      }
+    );
     // * -------------- Post jwt ----------------
+    // ! post jwt
     app.post("/jwt", (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.JWT, { expiresIn: "1d" });
