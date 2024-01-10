46e6a084909451e9cf160be746c6678d7a3d64a8
Muhammad Saikat <smsaikat000@gmail.com>
2024-01-10 15:11:10 +0600

    added 2 get route for calculator seller total income and manage chart

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-app.use(cors());
+const corsConfig = {
+  origin: "*",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+};
+app.use(cors(corsConfig));
 app.use(express.json());
 const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
@@ -47,12 +52,15 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
+    app.get("/health", (req, res) => {
+      res.send("welcome to Techouse");
+    });
     const Products = client.db("TecHouse").collection("Products");
     const Users = client.db("TecHouse").collection("Users");
     const AddToCart = client.db("TecHouse").collection("AddToCart");
     const SellerRequest = client.db("TecHouse").collection("SellerRequest");
     const Payment = client.db("TecHouse").collection("Payment");
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     //! ---------------------------Verify admin
     const VerifyAdmin = async (req, res, next) => {
       const email = req.decode.email;
@@ -74,7 +82,7 @@ async function run() {
       }
       next();
     };
-    
+
     //* ----------------- user related route --------------------
     // ! post user into data base
     app.post("/postUser", async (req, res) => {
@@ -82,7 +90,7 @@ async function run() {
       const result = await Users.insertOne(data);
       res.send(result);
     });
-   
+
     // ! get a single user
     app.get("/getUser/:email", async (req, res) => {
       const email = req.params.email;
@@ -124,18 +132,18 @@ async function run() {
       }
     );
     // * -------------------- Products related route --------------
-    // ! get products number 
-    app.get('/getProductNumber',async(req,res)=>{
-      const  result=await Products.find().toArray()
-      const product=result.length 
-      res.send({product})
-    })
+    // ! get products number
+    app.get("/getProductNumber", async (req, res) => {
+      const result = await Products.find().toArray();
+      const product = result.length;
+      res.send({ product });
+    });
     // ! get all Products
     app.get("/getAllProducts", async (req, res) => {
       const page = Number(req.query.page) || 1;
       const limit = Number(req.query.limit) || 5;
       const skip = (page - 1) * limit;
-      const result = await Products.find().skip(skip).limit(limit).toArray()
+      const result = await Products.find().skip(skip).limit(limit).toArray();
       res.send(result);
     });
     // ! get single product
@@ -146,27 +154,27 @@ async function run() {
       res.send(result);
     });
     // ! increase sell product sell number after payment
-    app.patch(`/incSellNumber/:id`,VerifyJwt,async(req,res)=>{
-      const id=req.params.id 
-      const   {quantity}=req.body
-      const query = {_id:new ObjectId(id)}
-      const findData=await Products.findOne(query)
-      const updatedDoc={
-        $set:{
-          selling:findData.selling+quantity,
-          quantity:findData.quantity-quantity
-        }
-      }
-      const result=await Products.updateOne(query,updatedDoc)
-      res.send(result)
-    })
+    app.patch(`/incSellNumber/:id`, VerifyJwt, async (req, res) => {
+      const id = req.params.id;
+      const { quantity } = req.body;
+      const query = { _id: new ObjectId(id) };
+      const findData = await Products.findOne(query);
+      const updatedDoc = {
+        $set: {
+          selling: findData.selling + quantity,
+          quantity: findData.quantity - quantity,
+        },
+      };
+      const result = await Products.updateOne(query, updatedDoc);
+      res.send(result);
+    });
     // ! get category base product
-    app.get('/getProductByCategory/:category',async(req,res)=>{
-      const category=req.params.category
-      const query={category}
-      const result=await Products.find(query).toArray()
-      res.send(result)
-    })
+    app.get("/getProductByCategory/:category", async (req, res) => {
+      const category = req.params.category;
+      const query = { category };
+      const result = await Products.find(query).toArray();
+      res.send(result);
+    });
     // * -------------------- Add To Cart related route ------------------------
     // ! add cart data into database
     app.put("/addToCard/:id", VerifyJwt, async (req, res) => {
@@ -203,7 +211,7 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          quantity:data.quantity+1
+          quantity: data.quantity + 1,
         },
       };
       const result = await AddToCart.updateOne(query, updatedDoc);
@@ -215,14 +223,14 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          quantity:data.quantity-1
+          quantity: data.quantity - 1,
         },
       };
-     
+
       const result = await AddToCart.updateOne(query, updatedDoc);
       res.send(result);
     });
-    // ! delete cart item 
+    // ! delete cart item
     app.delete("/deleteCart/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -231,18 +239,18 @@ async function run() {
     });
     // * ----------------------------- payment related route---------------------
     //  ! post payment
-    app.post('/payment',VerifyJwt,async(req,res)=>{
-      const data=req.body 
-      const result=await Payment.insertOne(data)
-      res.send(result)
-    })
-    // ! get user order product 
-    app.get(`/myOrder/:email`,VerifyJwt,async(req,res)=>{
-      const email=req.params.email 
-      const query={user:email}
-      const result=await Payment.find(query).toArray()
-      res.send(result)
-    })
+    app.post("/payment", VerifyJwt, async (req, res) => {
+      const data = req.body;
+      const result = await Payment.insertOne(data);
+      res.send(result);
+    });
+    // ! get user order product
+    app.get(`/myOrder/:email`, VerifyJwt, async (req, res) => {
+      const email = req.params.email;
+      const query = { user: email };
+      const result = await Payment.find(query).toArray();
+      res.send(result);
+    });
     // * ---------------------- Seller Related Route ---------------------
     // ! put Seller request
     app.put("/applySeller/:email", async (req, res) => {
@@ -291,40 +299,106 @@ async function run() {
         res.send(result);
       }
     );
-    // ! get seller's own products 
-    app.get('/sellerProducts/:email',VerifyJwt,verifySeller,async(req,res)=>{
-      const email=req.params.email
-      const query={sellerEmail:email}
-      const result=await Products.find(query).toArray()
-      res.send(result)
-    })
-    // ! get a single product for update
-    app.get('/updateProducts/:id',VerifyJwt,verifySeller,async(req,res)=>{
-      const id=req.params.id 
-      const query={_id:new ObjectId(id)}
-      const result=await Products.findOne(query)
-      res.send(result)
-    })
-    // ! update product by seller
-    app.patch('/updateProducts/:id',VerifyJwt,verifySeller,async(req,res)=>{
-      const id=req.params.id 
-      const query={_id:new ObjectId(id)}
-      const data=req.body
-      const updateDoc={
-        $set:{
-          ...data
-        }
+    // ! get seller total income
+    app.get(
+      `/getTotalIncome/:email`,
+      VerifyJwt,
+      verifySeller,
+      async (req, res) => {
+        const email = req.params.email;
+        const query = { seller: email };
+        const result = await Payment.find(query).toArray();
+        const money= result.reduce((accumulator, currentItem) => {
+          return accumulator + currentItem.totalPrice;
+        }, 0);
+        res.send({totalIncome:money})
       }
-      const result=await Products.updateOne(query,updateDoc)
-      res.send(result)
-    })
+    );
+    // ! get seller's own products
+    app.get(
+      "/sellerProducts/:email",
+      VerifyJwt,
+      verifySeller,
+      async (req, res) => {
+        const email = req.params.email;
+        const query = { sellerEmail: email };
+        const result = await Products.find(query).toArray();
+        res.send(result);
+      }
+    );
+    // ! get a single product for update
+    app.get(
+      "/updateProducts/:id",
+      VerifyJwt,
+      verifySeller,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await Products.findOne(query);
+        res.send(result);
+      }
+    );
+    // ! update product by seller
+    app.patch(
+      "/updateProducts/:id",
+      VerifyJwt,
+      verifySeller,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const data = req.body;
+        const updateDoc = {
+          $set: {
+            ...data,
+          },
+        };
+        const result = await Products.updateOne(query, updateDoc);
+        res.send(result);
+      }
+    );
     // ! delete a single product
-    app.delete('/deleteProduct/:id',VerifyJwt,verifySeller,async(req,res)=>{
-      const id=req.params.id 
-      const query={_id:new ObjectId(id)}
-      const result=await Products.deleteOne(query)
-      res.send(result)
-    })
+    app.delete(
+      "/deleteProduct/:id",
+      VerifyJwt,
+      verifySeller,
+      async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await Products.deleteOne(query);
+        res.send(result);
+      }
+    );
+    // ! get my all selling(payment) products
+    app.get(
+      `/getSellingData/:email`,
+      VerifyJwt,
+      verifySeller,
+      async (req, res) => {
+        const email = req.params.email;
+        const query = { seller: email };
+        const result = await Payment.find(query).toArray();
+        const pc = result.filter((pro) => pro.category === "pc");
+        const phone = result.filter((pro) => pro.category === "phone");
+        const accessory = result.filter((pro) => pro.category === "accessory");
+        const pcQuantity = pc.reduce((accumulator, currentItem) => {
+          return accumulator + currentItem.quantity;
+        }, 0);
+        const phoneQuantity = phone.reduce((accumulator, currentItem) => {
+          return accumulator + currentItem.quantity;
+        }, 0);
+        const accessoryQuantity = accessory.reduce(
+          (accumulator, currentItem) => {
+            return accumulator + currentItem.quantity;
+          },
+          0
+        );
+        res.send({
+          pc: pcQuantity,
+          phone: phoneQuantity,
+          accessory: accessoryQuantity,
+        });
+      }
+    );
     // * -------------- Post jwt ----------------
     // ! post jwt
     app.post("/jwt", (req, res) => {
